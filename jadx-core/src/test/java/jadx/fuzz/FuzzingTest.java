3e2feb1dd27a828a2f00ea5bedbc0d02e49fc4f2
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-10-21 14:22:07 +0100

    Move fuzzing test files to fuzz folder and remove unused imports

--- jadx-core/src/test/java/jadx/fuzz/FuzzingTest.java
+++ jadx-core/src/test/java/jadx/fuzz/FuzzingTest.java
@@ -1,24 +1,16 @@
-package jadx.api;
+package jadx.fuzz;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.List;
+import java.nio.file.Path;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import jadx.core.utils.files.FileUtils;
-import jadx.core.xmlgen.ResContainer;
-import jadx.plugins.input.dex.DexInputPlugin;
+import jadx.api.JadxArgs;
+import jadx.api.JadxDecompiler;
+import jadx.api.JavaClass;
+import jadx.fuzz.FuzzingTestImplementation.FuzzerClassFileTestDataSource;
 
 import static jadx.tests.api.utils.assertj.JadxAssertions.assertThat;
-import jadx.api.FuzzingTestImplementation.FuzzerClassFileTestDataSource;
 
 public class FuzzingTest {
 
